82c086ae695c7414a17e9df48a77402e13a1cd22
idleberg <git@idleberg.com>
2021-04-20 22:42:42 +0200

    use which module

--- src/provider.js
+++ src/provider.js
@@ -1,9 +1,9 @@
-import meta from '../package.json';
 import { configSchema, getConfig } from './config';
 import { EventEmitter } from 'events';
 import { satisfyDependencies } from 'atom-satisfy-dependencies';
-import { spawnSync } from 'child_process';
-import { which } from './util';
+import Logger from './log';
+import meta from '../package.json';
+import which from 'which';
 
 export { configSchema as config };
 
@@ -21,14 +21,17 @@ export function provideBuilder() {
 
     isEligible() {
       if (getConfig('alwaysEligible') === true) {
+        Logger.log('Always eligible');
         return true;
       }
 
-      const cmd = spawnSync(which(), ['decaffeinate']);
+      if (which.sync('decaffeinate', { nothrow: true })) {
+        Logger.log('Build provider is eligible');
+        return true;
+      }
 
-      return !cmd?.stdout?.toString()
-        ? false
-        : true;
+      Logger.error('Build provider isn\'t eligible');
+      return false;
     }
 
     settings() {
@@ -47,9 +50,15 @@ export function provideBuilder() {
   };
 }
 
-// This package depends on build, make sure it's installed
 export function activate() {
+  Logger.log('Activating package');
+
+  // This package depends on build, make sure it's installed
   if (getConfig('manageDependencies') === true) {
     satisfyDependencies(meta.name);
   }
+}
+
+export function deactivate() {
+  Logger.log('Deactivating package');
 }
